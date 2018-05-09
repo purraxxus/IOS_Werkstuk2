d1ff558682f025ff3e1f541a80cafa61e60e5372
Purraxxus <thatcronosdude@outlook.com>
2018-05-09 15:27:13 +0200

    JSON ophalen werkt, staat in Array

--- IOS_Werkstuk2/HomeViewController.swift
+++ IOS_Werkstuk2/HomeViewController.swift
@@ -9,27 +9,31 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var villoStations: [VilloStation] = readStationInformation(readStationsFromDatabase())
-        writeToMap(villoStations)
+        let urlString = "https://api.jcdecaux.com/vls/v1/stations?apiKey=c7e226e356101d8a519809679aba25090209ca8e"
+        let url = URL(string: urlString)
+        let urlRequest = URLRequest(url: url!)
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        let task = session.dataTask(with: urlRequest) { (data, response, error) in
+            guard error == nil else {
+                print("error GET")
+                print(error!)
+                return
+            }
+            guard let responseData = data else {
+                print("no data")
+                return
+            }
+            guard let villoData = try? JSONSerialization.jsonObject(with: responseData, options: []) as! NSArray else {
+                print("failed JSONSerialization")
+                return
+            }
+            
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    func readStationsFromDatabase(){
-        //connectie met database maken
-        //Schrijf stationsNamen in array
-        //return array
-    }
-    
-    func readStationInformation(array:Array<String>){
-        //connectie met database maken
-        //return array met alle informatie over arrays in
-    }
-    
-    func writeToMap(array:Array<VilloStation>){
-        //lees locaties uit array
-        //schrijf op kaart
-    }
+
 }
