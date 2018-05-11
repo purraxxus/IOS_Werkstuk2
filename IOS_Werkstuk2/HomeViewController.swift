668e1cadcbced38d635505264c29fd459a91b7b1
Purraxxus <thatcronosdude@outlook.com>
2018-05-11 13:55:11 +0200

    verder werken aan annotations

--- IOS_Werkstuk2/HomeViewController.swift
+++ IOS_Werkstuk2/HomeViewController.swift
@@ -61,8 +61,25 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate, MKMapView
                 return
             }
             for value in villoData {
-                let name = value["name"] as? String
-                let annotation = Annotation(title: name!, subtitle: "test", coordinate: CLLocationCoordinate2DMake(50.7756865,4.19168370))
+                let enter = "\n"
+                let stands = "available stands: "
+                let bikes = "available bikes: "
+                let name = value["name"] as? String ?? "no name available"
+                let status = value["status"] as? String ?? "no status available"
+                let address = value["address"] as? String ?? "no address available"
+                let available_bike_stands = value["available_bike_stands"] as? String ?? "no bike stands data available"
+                let available_bikes = value["available_bikes"] as? String ?? "no bike data available"
+                var lat:Double = 0.0
+                var lng:Double = 0.0
+                for (pos,posValue) in (value["position"] as? NSDictionary)!{
+                    if pos as! String == "lat"{
+                        lat = posValue as! Double
+                    }
+                    if pos as! String == "lng"{
+                        lng = posValue as! Double
+                    }
+                }
+                let annotation = Annotation(title: name, subtitle: address + enter + status + enter + stands + available_bike_stands + enter + bikes + available_bikes, coordinate: CLLocationCoordinate2DMake(lat,lng))
                 self.annotations.append(annotation)
             }
             self.dispatchGroup.leave()
